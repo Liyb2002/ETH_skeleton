c00c9e0faaf7098c9ebe75edf41a319ebc2d9fbe
Liyb2002 <55519796+Liyb2002@users.noreply.github.com>
2022-01-14 02:00:53 -0500

    goroutine

--- main.go
+++ main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
-//	"fmt"
+	"fmt"
+	"strconv"
 	"eth/core"
 	"eth/consensus"
+	"sync"
 )
 
 func main(){
 	chain := core.CreateNewChain()
+	var wg sync.WaitGroup
 
-	miner := consensus.CreateMiner("miner1")
-	miner.Work("hi", chain)
-	miner.Work("hi23", chain)
-	miner.Work("hdsfai", chain)
+	for w:=0; w<3; w++{
+		w := w
+		wg.Add(1)
+		go func(){
+			defer wg.Done()
+			fmt.Println(strconv.Itoa(w))
+			name := "miner"+ strconv.Itoa(w)
+			miner := consensus.CreateMiner(name)
+			miner.Work("work!", chain)
+		}()
+	}
+	wg.Wait()
+
 	core.ViewBlockchain(chain)
 }
