b8510824322971ce93d521969004f353a9b0d0a1
Liyb2002 <55519796+Liyb2002@users.noreply.github.com>
2022-01-14 02:30:51 -0500

    runminer

---
+++ consensus/runMiner.go
@@ -0,0 +1,35 @@
+package consensus
+
+import (
+	"strconv"
+	"fmt"
+	"eth/core"
+)
+
+func RunMiner(bc *core.Blockchain, numberOfBlocks int, numberOfMiners int){
+	done := make(chan bool)
+	jobs := make(chan string)
+
+	for w:=0; w<numberOfMiners; w++{
+		w:= w
+		go func(){
+			miner := CreateMiner("miner"+ strconv.Itoa(w))
+
+			for{
+				j, more := <- jobs
+				if more{
+					miner.Work(j, bc)
+				}else{
+					fmt.Println("done all jobs")
+					done <- true
+				}
+			}
+		}()
+	}
+
+	for j:=0; j<numberOfBlocks; j++{
+		jobs <- strconv.Itoa(j)
+	}
+	close(jobs)
+	<- done
+}
--- main.go
+++ main.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
+//	"fmt"
 	"eth/core"
 	"eth/consensus"
 //	"sync"
@@ -10,32 +9,6 @@ import (
 
 func main(){
 	chain := core.CreateNewChain()
-	done := make(chan bool)
-	jobs := make(chan string)
-
-	for w:=0; w<3; w++{
-		w:= w
-		go func(){
-			miner := consensus.CreateMiner("miner"+ strconv.Itoa(w))
-
-			for{
-				j, more := <- jobs
-				if more{
-					miner.Work(j, chain)
-				}else{
-					fmt.Println("done all jobs")
-					done <- true
-				}
-			}
-		}()
-	}
-
-	for j:=0; j<20; j++{
-		jobs <- strconv.Itoa(j)
-	}
-
-	close(jobs)
-	<- done
-
+	consensus.RunMiner(chain, 10, 5)
 	core.ViewBlockchain(chain)
 }
