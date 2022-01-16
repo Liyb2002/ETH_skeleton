c1f0534a9ac7ae641b1952cffe2700cc1c46619d
Liyb2002 <55519796+Liyb2002@users.noreply.github.com>
2022-01-15 23:26:15 -0500

    create_TX

--- core/txPool.go
+++ core/txPool.go
@@ -1,11 +1,41 @@
 package core
 
+import(
+	"fmt"
+	"time"
+)
+
 type Tx struct{
 	sender string
 	recipient string
-	amount int
+	amount int64
 
 }
 type TxPool struct{
-	var txList [10] *Tx
+	timeStamp time.Time
+	txList [] *Tx
+}
+
+
+func CreateNewTx(sender string, recipient string, amount int64)*Tx{
+	return &Tx{sender, recipient, amount}
+}
+
+func CreateNewTxPool()*TxPool{
+	gensisTx := CreateNewTx("gensis", "gensis", 0)
+	return &TxPool{timeStamp:time.Now(), txList:[] *Tx{gensisTx} }
+}
+
+func AddTxToPool(pool *TxPool, txInstance *Tx){
+	fmt.Println("hi")
+}
+
+
+//View functions
+func ViewTxPool(pool *TxPool){
+	fmt.Println("hi")
+}
+
+func ViewTx(tx *Tx){
+	fmt.Println("sender is", tx.sender, "recipient is", tx.recipient, "amount is", tx.amount)
 }
--- main.go
+++ main.go
@@ -3,12 +3,18 @@ package main
 import (
 //	"fmt"
 	"eth/core"
-	"eth/consensus"
+//	"eth/consensus"
 //	"sync"
 )
 
 func main(){
+	/*
 	chain := core.CreateNewChain()
 	consensus.RunMiner(chain, 10, 5)
 	core.ViewBlockchain(chain)
+	*/
+
+	NewTx := core.CreateNewTx("yb", "hjh", 10)
+	core.ViewTx(NewTx)
+
 }
